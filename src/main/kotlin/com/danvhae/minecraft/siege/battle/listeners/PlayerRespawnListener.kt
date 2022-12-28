ee906a0e08980575139071ce087d6bf5af5404ad
RomeoRKPK <mindaye8504@gmail.com>
2022-12-28 17:01:52 +0900

    Feat: 작업 진행 중

---
+++ src/main/kotlin/com/danvhae/minecraft/siege/battle/listeners/PlayerRespawnListener.kt
@@ -0,0 +1,19 @@
+package com.danvhae.minecraft.siege.battle.listeners
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerRespawnEvent
+
+class PlayerRespawnListener : Listener {
+
+    @EventHandler
+    fun onPlayerDeath(event:PlayerDeathEvent){
+
+    }
+
+    @EventHandler
+    fun onPlayerRespawn(event:PlayerRespawnEvent){
+
+    }
+}
