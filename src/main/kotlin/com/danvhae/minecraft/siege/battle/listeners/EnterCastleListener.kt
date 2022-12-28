481ff93ac6637518f3c3b77c93f080eacb82774f
RomeoRKPK <mindaye8504@gmail.com>
2022-12-28 15:41:55 +0900

    feat: 별 진입 진출 테스트 중

--- src/main/kotlin/com/danvhae/minecraft/siege/battle/listeners/EnterCastleListener.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/battle/listeners/EnterCastleListener.kt
@@ -2,7 +2,9 @@ package com.danvhae.minecraft.siege.battle.listeners
 
 import com.danvhae.minecraft.siege.core.enums.SiegeCastleStatus
 import com.danvhae.minecraft.siege.core.events.EnterCastleEvent
+import com.danvhae.minecraft.siege.core.objects.SiegeCastle
 import com.danvhae.minecraft.siege.core.objects.SiegePlayer
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
@@ -18,7 +20,10 @@ class EnterCastleListener : Listener {
         if(castle.owner == null)return
         event.siegePlayer?:return
         if(event.siegePlayer!!.team == castle.ownerPlayer().team)return
+        Bukkit.getLogger().info("EnterTeam:${event.siegePlayer!!.team}")
+        Bukkit.getLogger().info("OwnerTeam:${castle.ownerPlayer().alias}")
         castle.status = SiegeCastleStatus.UNDER_BATTLE
+        Bukkit.getLogger().info("${castle.name} : ${SiegeCastle.DATA[castle.id]!!.status}")
     }
 
 }
--- src/main/kotlin/com/danvhae/minecraft/siege/battle/listeners/LeaveCastleListener.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/battle/listeners/LeaveCastleListener.kt
@@ -2,6 +2,7 @@ package com.danvhae.minecraft.siege.battle.listeners
 
 import com.danvhae.minecraft.siege.core.enums.SiegeCastleStatus
 import com.danvhae.minecraft.siege.core.events.LeaveCastleEvent
+import com.danvhae.minecraft.siege.core.objects.SiegeCastle
 import com.danvhae.minecraft.siege.core.objects.SiegePlayer
 import com.danvhae.minecraft.siege.core.utils.PlayerUtil
 import org.bukkit.Bukkit
@@ -12,16 +13,24 @@ class LeaveCastleListener : Listener {
 
     @EventHandler
     fun onLeaveCastle(event:LeaveCastleEvent){
+        //Bukkit.getLogger().info("leaving ${event.castle.name}")
         val castle = event.castle
         if(castle.status != SiegeCastleStatus.UNDER_BATTLE)return
+        val ownerTeam = castle.ownerPlayer().team
+        val leaverTeam = event.siegePlayer?.team?:return
+        if(ownerTeam == leaverTeam)return
+        //Bukkit.getLogger().info("line 21")
         for(player in Bukkit.getOnlinePlayers()){
             if(player.uniqueId == event.player.uniqueId)continue
             val region = PlayerUtil.playerRegion(player)
             val siege = SiegePlayer.DATA[player.uniqueId]?:continue
-            if(castle.ownerPlayer().team == siege.team)continue
+            if(siege.team == ownerTeam)continue
+
             if(castle.worldGuardID in region)return
         }
 
         castle.status = SiegeCastleStatus.PEACEFUL
+        Bukkit.getLogger().info("PEACE ${castle.name} : ${SiegeCastle.DATA[castle.id]!!.status}")
+
     }
 }
