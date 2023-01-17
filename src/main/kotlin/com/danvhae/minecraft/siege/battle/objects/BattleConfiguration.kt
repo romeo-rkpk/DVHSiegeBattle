0c5b326f998cd48e85b6bbc74ec6bb99360bd4c6
RomeoRKPK <mindaye8504@gmail.com>
2023-01-17 13:56:50 +0900

    Fix: 화살 데미지 계산 모델 기본갑 변경

--- src/main/kotlin/com/danvhae/minecraft/siege/battle/objects/BattleConfiguration.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/battle/objects/BattleConfiguration.kt
@@ -2,5 +2,5 @@ package com.danvhae.minecraft.siege.battle.objects
 
 import com.danvhae.minecraft.siege.battle.enums.ArrowDamageModel
 
-class BattleConfiguration(internal var arrow:ArrowDamageModel = ArrowDamageModel.NOT_CONTROL) {
+class BattleConfiguration(internal var arrow:ArrowDamageModel = ArrowDamageModel.KINETIC_ENERGY) {
 }
