7a0858b09cf1b5ca54457fbf12ed6e44f6a54495
RomeoRKPK <mindaye8504@gmail.com>
2023-01-20 06:24:28 +0900

    Feat: 스코어보드 이름 표시 여부 제어 시도 중

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.danvhae.minecraft.siege.battle"
-version = "1.2.0-a1"
+version = "1.2.1-a1"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/danvhae/minecraft/siege/battle/utils/ScoreBoardUtil.kt
+++ src/main/kotlin/com/danvhae/minecraft/siege/battle/utils/ScoreBoardUtil.kt
@@ -6,11 +6,16 @@ import org.bukkit.scoreboard.Team
 class ScoreBoardUtil {
 
     companion object{
-        fun nameVisible(show:Boolean){
+        fun nameVisible(siege:Boolean){
             val scoreboard = Bukkit.getScoreboardManager().mainScoreboard
             scoreboard.teams.forEach{team ->
+
                 team.setOption(Team.Option.NAME_TAG_VISIBILITY,
-                    if(show) Team.OptionStatus.ALWAYS else Team.OptionStatus.FOR_OWN_TEAM)
+                    if(!siege) Team.OptionStatus.ALWAYS else Team.OptionStatus.FOR_OTHER_TEAMS
+                )
+                    //Team.OptionStatus.NEVER
+
+
             }
         }
     }
